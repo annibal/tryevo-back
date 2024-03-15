9de4239fc8034e16aff1e09f739d44076cc79c27
Arthur Annibal <arthur.annibal@gmail.com>
2024-03-15 17:35:01 -0300

    fix precoPag formatter

--- src/helpers/precoformater.js
+++ src/helpers/precoformater.js
@@ -1,5 +1,5 @@
 module.exports = function precoPag(floatValue) {
-  const fixed = floatValue.toFixed(2);
+  const fixed = (+floatValue).toFixed(2);
   const multipliedValue = fixed * 100;
   const intValue = Math.floor(multipliedValue);
 
